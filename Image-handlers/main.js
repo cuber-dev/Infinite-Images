142fabdd31b679324c4ab6ca25e374057050e76e
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-04-01 23:29:41 +0530

    hdhdhhdd

--- Image-handlers/main.js
+++ Image-handlers/main.js
@@ -1,6 +1,8 @@
-/*
 const pickedImage = document.querySelector('#picked-image');
 const pickedImgTitle = document.querySelector('#picked-image-title');
+
+const backBtn = document.querySelector('#back-btn');
+
 
 const urlParams = new URLSearchParams(window.location.search);
 const image = urlParams.get('image');
@@ -8,5 +10,8 @@ const title = urlParams.get('title');
 
 pickedImage.src = image;
 pickedImgTitle.innerText = title;
-*/
 
+
+backBtn.addEventListener('click',() => {
+  window.history.back();
+});
