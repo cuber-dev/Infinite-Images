e8fb54e90c76817bdf0c8e6ddb538cac57d6026c
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-04-01 18:15:34 +0530

    ysysyyss

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const debounceRequest = debounceTimeout(makeRequest,1500);
 
 /* Making api request */
 async function makeRequest(photoName) {
-  const url = 'https://api.unsplash.com/search/photos?query='+ photoName +'&per_page=5&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+  const url = 'https://api.unsplash.com/search/photos?query='+ photoName +'&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
   const responose = await fetch(url);
   const data = await responose.json();
   addImagesElements(data);
